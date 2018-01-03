b79b7987900ce6d5fc2895e11481cdcd3fed0735
Maxwell Brown <maxwellgbrown@gmail.com>
2018-01-03 05:48:21 -0500

    setup testing & setuptools testing

--- setup.py
+++ setup.py
@@ -2,16 +2,21 @@
 
 from setuptools import setup
 
+install_requires = ["Sphinx==1.6.5"]
+tests_require = install_requires + ["pytest==3.3.1"]
 
 setup(
     name="sphinx-websequencediagrams",
     version="0.0",
     license="MIT",
     url="https://github.com/MaxwellGBrown/sphinx-websequencediagrams",
+    install_requires=install_requires,
+    tests_require=tests_require,
+    setup_requires=["pytest-runner>=2.0"],
+    py_modules=["sphinx_websequencediagrams"],
     entry_points={
         "sphinx.builders": [
-            "sphinx-websequencediagrams = sphinx_wsd"
+            "sphinx-websequencediagrams = sphinx_websequencediagrams"
         ],
     },
-    py_modules=["sphinx_websequencediagrams"]
 )
---
+++ test_sphinx_websequencediagrams.py
@@ -0,0 +1,6 @@
+"""Tests for sphinx_websequencediagrams.py."""
+
+
+def test_hello_world():
+    """Assert Testing framework is functional."""
+    assert True
