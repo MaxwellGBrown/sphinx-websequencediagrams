8db5d21035fc432bca60282059d5b25410f416e2
Maxwell Brown <maxwellgbrown@gmail.com>
2018-03-02 16:20:27 -0500

    Using README.rst as long_description

---
+++ README.rst
@@ -0,0 +1,30 @@
+sphinx-websequencediagrams
+==========================
+
+A Sphinx Directive for creating Sequence Diagrams using `www.websequencediagrams.com`__
+
+Setup
+-----
+
+1. Install the package
+
+::
+
+  pip install git+https://github.com/MaxwellGBrown/sphinx-websequencediagrams
+
+
+2. Update your sphinx ``conf.py`` to include the package
+
+::
+
+  extensions = ["sphinx_websequencediagrams"]
+
+
+3. Use the ``sequencediagram`` directive in your RST!
+
+::
+
+  .. sequencediagram:
+  
+     A->B: A to B
+     B->A: B to A
--- setup.py
+++ setup.py
@@ -4,15 +4,21 @@ from setuptools import setup
 
 install_requires = [
     "demjson==2.2.4",
+]
+tests_require = install_requires + [
+    "pytest==3.3.1",
     "Sphinx==1.6.5",
 ]
-tests_require = install_requires + ["pytest==3.3.1"]
 
 setup(
     name="sphinx-websequencediagrams",
     version="0.1",
     license="MIT",
     url="https://github.com/MaxwellGBrown/sphinx-websequencediagrams",
+    description="Sphinx extension that integrates www.websequencediagrams.com",
+    long_description=open('README.rst').read(),
+    author="Maxwell Brown",
+    author_email="maxwellgbrown@gmail.com",
     install_requires=install_requires,
     tests_require=tests_require,
     setup_requires=["pytest-runner>=2.0"],
