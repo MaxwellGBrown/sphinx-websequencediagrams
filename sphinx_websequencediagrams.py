1c710ff16be0f956e164432e9fc57d0f8bbd99cc
Maxwell Brown <maxwellgbrown@gmail.com>
2018-03-06 11:26:25 -0500

    Fixed downloads in relative locations
      Also moved away from the image node because that thing sucked

--- sphinx_websequencediagrams.py
+++ sphinx_websequencediagrams.py
@@ -11,15 +11,20 @@ from docutils import nodes
 from docutils.parsers.rst import Directive
 from docutils.parsers.rst import directives
 from sphinx.util import logging
+from sphinx.util.osutil import ensuredir
 
 
 log = logging.getLogger(__name__)
 
 
 class WebSequenceDiagram(object):
-    """Manages interaction with WebSequenceDiagrams."""
+    """Manages interaction with www.websequencediagrams.com.
 
-    api_url = "http://www.websequencediagrams.com/"
+    This class's only responsibility is to ensure that requests made to the
+    api are made correctly, and that the responses are read into an io stream.
+    """
+
+    _api_url = "http://www.websequencediagrams.com/"
 
     class WebSequenceDiagramError(Exception):
         """Exception for any errors returned by www.websequencediagrams.com."""
@@ -40,7 +45,7 @@ class WebSequenceDiagram(object):
     def create_diagram(self):
         """Create diagram via websequencediagram.com. Returns diagram URL."""
         body = urllib.parse.urlencode(self.request_body).encode()
-        with urllib.request.urlopen(self.api_url, body) as response_io:
+        with urllib.request.urlopen(self._api_url, body) as response_io:
             response_body = response_io.read().decode()
 
         # The response is a JavaScript object instead of a JSON object
@@ -49,7 +54,7 @@ class WebSequenceDiagram(object):
         if response.get("errors"):
             raise self.WebSequenceDiagramError(response["errors"])
 
-        return urllib.parse.urljoin(self.api_url, response["img"])
+        return urllib.parse.urljoin(self._api_url, response["img"])
 
     def __enter__(self):
         """Create and operate on a diagram as a file-like object."""
@@ -59,34 +64,6 @@ class WebSequenceDiagram(object):
     def __exit__(self, *args):
         """Close any open URL connections made for retrieving diagrams."""
         return  # the responses from urllib.request.urlopen close themselves
-
-
-class sequencediagram(nodes.image):
-    """Node class for sequencediagram directive that inherits from image."""
-
-    pass
-
-
-def visit_sequencediagram_node(self, node):
-    """Start node context for a sequencediagram.
-
-    For example, the opening html tag
-    --> <p>
-          Some Text
-        </p>
-    """
-    self.visit_image(node)
-
-
-def depart_sequencediagram_node(self, node):
-    """Close node context for a sequencediagram.
-
-    For example, the opening html tag
-        <p>
-          Some Text
-    --> </p>
-    """
-    self.depart_image(node)
 
 
 class SequenceDiagramDirective(Directive):
@@ -134,44 +111,83 @@ class SequenceDiagramDirective(Directive):
         node = sequencediagram()
 
         filename = "{}.{}".format(target_id, self.options["format"])
-        source_filepath = os.path.join(env.app.srcdir, "_static", filename)
+        ensuredir(
+            os.path.join(env.app.builder.outdir, env.app.builder.imagedir)
+        )
+        build_filepath = os.path.join(
+            env.app.builder.outdir,
+            env.app.builder.imagedir,
+            filename,
+        )
 
+        log.info("Downloading %s", build_filepath)
         with WebSequenceDiagram(text_diagram, **self.options) as http_diagram:
-            with open(source_filepath, "wb") as source_diagram:
+            with open(build_filepath, "wb") as source_diagram:
                 shutil.copyfileobj(http_diagram, source_diagram)
 
-        # Create/Update env.source_diagrams for cleanup later
-        source_diagrams = getattr(env, "source_diagrams", list())
-        source_diagrams.append(source_filepath)
-        env.source_diagrams = source_diagrams
+        # Set src relative to build path & document build location
+        relative_location = os.path.relpath(
+            env.srcdir, os.path.dirname(env.doc2path(env.docname))
+        )
+        node["src"] = os.path.join(
+            relative_location,
+            os.path.relpath(build_filepath, env.app.outdir),
+        )
 
-        node['uri'] = os.path.relpath(source_filepath, env.app.srcdir)
+        node["uri"] = os.path.relpath(build_filepath, env.app.outdir)
         node["alt"] = self.options.get("alt", target_id)
         return [target_node, node]
 
 
-def purge_sequencediagrams(app, env, docname):
-    """Clear any data in the environment persistant env related to docname."""
+# -----------------------------------------------------------------------------
+# Docutil Nodes used to handle .. sequencediagram::
+# -----------------------------------------------------------------------------
+class sequencediagram(nodes.literal):
+    """Node class for sequencediagram directive that inherits from literal.
+
+    Opted NOT to inherit from nodes.image because there's too much
+    baggage coming from Sphinx processing image nodes.
+    """
+
     pass
 
 
-def process_sequencediagram_nodes(app, doctree, fromdocname):
-    """Extra sequencediagram node processing after all nodes are created."""
+def visit_sequencediagram_node(self, node):
+    """Start node context for a sequencediagram.
+
+    For example, the opening html tag...
+    --> <p>
+          Some Text
+        </p>
+    """
+    # Simplified from docutils.writers._html_base.HTMLTranslator.visit_image
+    attrs = {"src": node["src"], "alt": node["alt"]}
+    self.body.append(self.emptytag(node, "img", "\n", **attrs))
+
+
+def depart_sequencediagram_node(self, node):
+    """Close node context for a sequencediagram.
+
+    For example, the closing html tag...
+        <p>
+          Some Text
+    --> </p>
+    """
     pass
+# -----------------------------------------------------------------------------
 
 
 def setup(app):
     """Add the sequencediagram directive to Sphinx."""
     app.add_config_value("include_sequencediagrams", True, "html")
 
+    # custom directive for parsing ..sequencediagram:: from .rst
+    app.add_directive("sequencediagram", SequenceDiagramDirective)
+
+    # custom node for writing sequencediagram directive to HTML
     app.add_node(
         sequencediagram,
         html=(visit_sequencediagram_node, depart_sequencediagram_node),
     )
-
-    app.add_directive("sequencediagram", SequenceDiagramDirective)
-
-    app.connect("doctree-resolved", process_sequencediagram_nodes)
-    app.connect("env-purge-doc", purge_sequencediagrams)
 
     return {'version': '0.0'}  # Identify version of the extension in return
