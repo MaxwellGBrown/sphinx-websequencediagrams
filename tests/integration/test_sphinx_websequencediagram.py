9dba045b4a905ed88794382869a1bf8e439a20ed
Maxwell Brown <maxwellgbrown@gmail.com>
2018-08-08 16:42:46 -0400

    Add test generated images against static images

--- tests/integration/test_sphinx_websequencediagram.py
+++ tests/integration/test_sphinx_websequencediagram.py
@@ -3,46 +3,65 @@ import os
 import os.path
 import subprocess
 
+from bs4 import BeautifulSoup
 import pytest
 
 
 pytestmark = [pytest.mark.integration]
 
 
-@pytest.fixture
-def fixture_path():
-    """Return the fixture path for integration test fixtures."""
-    integration_tests_directory = os.path.dirname(os.path.realpath(__file__))
-    return os.path.join(integration_tests_directory, 'fixtures')
+FIXTURES_PATH = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),  # abs path of this file
+    'fixtures',
+)
+
+DOCS_FIXTURE_PATH = os.path.join(FIXTURES_PATH, 'test_docs')
+SOURCE_PATH = os.path.join(DOCS_FIXTURE_PATH, 'source')
+BUILD_PATH = os.path.join(DOCS_FIXTURE_PATH, 'build')
+
+IMAGES_FIXTURE_PATH = os.path.join(FIXTURES_PATH, 'images')
 
 
 @pytest.fixture
-def build_docs(request, fixture_path):
+def build_docs(request):
     """Return a function that builds the documentation."""
-    docs_path = os.path.join(fixture_path, 'test_docs')
-
-    source_path = os.path.join(docs_path, 'source')
-    build_path = os.path.join(docs_path, 'build')
-
     def _clean_up_build():
-        subprocess.call(['rm', '-rf', build_path])
+        subprocess.call(['rm', '-rf', BUILD_PATH])
 
     request.addfinalizer(_clean_up_build)
 
     def build_docs():
         """Build test sphinx documentation & return HTML of build."""
-        subprocess.call(['sphinx-build', source_path, build_path])
+        subprocess.call(['sphinx-build', SOURCE_PATH, BUILD_PATH])
 
-        build_file = os.path.join(build_path, 'index.html')
+        build_file = os.path.join(BUILD_PATH, 'index.html')
         with open(build_file, 'r') as output_file:
-            return output_file.read()
+            raw_html = output_file.read()
+
+        return BeautifulSoup(raw_html, 'html.parser')
 
     return build_docs
 
 
 def test_sphinx_websequencediagrams(build_docs):
     """Assert sphinx_websequencediagrams can build sequence diagrams."""
-    raw_html = build_docs()
-    # TODO Compare the <img src=URL /> to a static file
-    assert 'sequencediagram-index-0' in raw_html
-    assert 'sequencediagram-index-1' in raw_html
+    html = build_docs()
+
+    # based on tests/interation/fixtures/test_docs/source/index.rst
+    inline_sequence_diagram = html.find("img", id="sequencediagram-index-0")
+    file_sequence_diagram = html.find("img", id="sequencediagram-index-1")
+
+    assert inline_sequence_diagram["src"] == './_images/sequencediagram-index-0.png'  # noqa
+    assert file_sequence_diagram["src"] == './_images/sequencediagram-index-1.png'  # noqa
+
+    # compare file contents to make sure the created image isn't malformed
+    generated_vs_expected = [
+        ('sequencediagram-index-0.png', 'inline_sequence_diagram.png'),
+        ('sequencediagram-index-1.png', 'file_sequence_diagram.png'),
+    ]
+    for generated, expected in generated_vs_expected:
+        generated_filepath = os.path.join(BUILD_PATH, '_images', generated)
+        expected_filepath = os.path.join(IMAGES_FIXTURE_PATH, expected)
+        with open(generated_filepath, 'rb') as generated_image:
+            with open(expected_filepath, 'rb') as expected_image:
+                assert generated_image.read() == expected_image.read()
